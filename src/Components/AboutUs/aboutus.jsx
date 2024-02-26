7ce00d925b2bcc6b0b1e3f6d6d980f2bf42ea524
chamindu_a.15 <chamindua15@gmail.com>
2024-02-26 15:54:43 +0530

    About us

--- src/Components/AboutUs/aboutus.jsx
+++ src/Components/AboutUs/aboutus.jsx
@@ -1,22 +1,71 @@
-import React from 'react'
+import React from "react";
 import "./aboutus.css";
 
 const aboutus = () => {
   return (
-    <div className='aboutus-bg'>
+    <div className="aboutus-bg">
+      <div>
+        <div className="aboutus-title">
+          <span>Welcome to CraftBay</span>
+        </div>
         <div>
-            <span className="aboutus-title">Welcome to CraftBay</span>
-        <div className="aboutus-description-container">
-        
+          <div className="aboutus-description-container">
+            <div className="about-us-text">
+              <div >
+              <span>
+                &emsp; Welcome to CraftBay, a haven for lovers of all things
+                handmade and unique. CraftBay was born from a simple yet
+                powerful idea: to create a platform that celebrates the
+                incredible craftsmanship of artisans from every corner of the
+                globe. Our mission is to connect talented creators with
+                passionate customers, fostering a community built on
+                appreciation for the artistry, dedication, and creativity that
+                goes into each handmade piece.
+              </span>
+              </div>
 
-        </div>
-        </div>
+              <div className="p1">
+              <span>
+              &emsp; CraftBay is more than just a marketplace where it's a vibrant
+                hub where artisans can share their stories, cultures, and
+                traditions through their craft. From intricate textiles woven
+                with generations of expertise to delicate ceramics molded with
+                care and precision, each item on CraftBay tells a tale of
+                passion and dedication.
+              </span>
+              </div>
 
-        <div className="aboutus-img-container">
+              <div className="p1">
+              <span>
+              &emsp; We believe in the power of handmade to enrich lives and create
+                meaningful connections. By supporting artisans, we not only
+                celebrate their talents but also uphold traditions, preserve
+                cultural heritage, and promote sustainability. Every purchase
+                made on CraftBay directly supports these artisans, empowering
+                them to continue pursuing their craft and sustaining their
+                livelihoods.
+              </span>
+              </div>
 
+              <div className="p1">
+              <span>
+              &emsp; At CraftBay, we're committed to curating a diverse collection of
+                high quality handmade goods that reflect the rich tapestry of
+                global artistry. Whether you're searching for a unique statement
+                piece to adorn your home or a heartfelt gift that tells a story,
+                CraftBay offers an unparalleled selection of treasures waiting
+                to be discovered.
+              </span>
+              </div>
+            </div>
+          </div>
         </div>
+      </div>
+      <div>
+        <div className="aboutus-img-container"></div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default aboutus
+export default aboutus;
